f22a981b84f40839bcdf8df69eb502b490f38217
lizsPC <lizzyhllrn@gmail.com>
2023-11-26 20:15:37 -0700

    changed buildroot branch

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 790d22e7d68e6a2d5e8965d0e791b22800af1d3f
+AESD_ASSIGNMENTS_VERSION = f51725c229b2f75b1134f792c414d3439464fd03
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,11 +16,13 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-simple $(TARGET_DIR)/usr/bin/
+
 endef
 
 $(eval $(generic-package))
