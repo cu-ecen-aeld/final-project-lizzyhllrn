aba6c2dc93a605e19047d16ee82e354472770660
lizsPC <lizzyhllrn@gmail.com>
2023-11-30 10:39:13 -0700

    server-client motion interaction complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 00430873ab5f772734c315606482a095285aa634
+AESD_ASSIGNMENTS_VERSION = 7d86a210d2332928688b148278f91889e154723d
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,15 +15,15 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/capture all
-
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 
 
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/capture/capture $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-simple $(TARGET_DIR)/usr/bin/
+
 
 
 
